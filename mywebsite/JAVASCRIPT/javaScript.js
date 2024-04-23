a834d79f8ce32640c82e92c8dcc5dbe83d047409
DhanushYarragunta <158791996+DhanushYarragunta@users.noreply.github.com>
2024-04-23 16:04:33 +0530

    Add files via upload

---
+++ mywebsite/JAVASCRIPT/javaScript.js
@@ -0,0 +1,9 @@
+let p=document.querySelector("p");
+p.setAttribute("class","rcb");
+const f=()=>{
+    console.log(890);
+};
+let d=document.querySelector("button");
+d.addEventListener(onclick,()=>{
+    console.log(890);
+});
